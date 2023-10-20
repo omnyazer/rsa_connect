30d6d9c401cc476b59c0c7f5138962b37e482f58
zergho_o <zergho_o@etna-alternance.net>
2023-10-20 20:08:03 +0200

    modal button

--- rsa_connect/src/components/TitreOffre.js
+++ rsa_connect/src/components/TitreOffre.js
@@ -3,11 +3,12 @@ import TextDescriptionInput from './TextDescriptionInput';
 
 function TitreOffre({ onTitleChange }) {
   return (
-    <div className="bg-white rounded-md shadow-md my-4 text-center shadow-[#000091] ">
+    <div className="bg-white rounded-md shadow-md my-4 text-center shadow-[#000091]">
       <TextDescriptionInput
         title={null}
         onTitleChange={onTitleChange}
         placeholder="Titre de l'offre"
+        className="text-[#000091]" 
       />
     </div>
   );
--- rsa_connect/src/components/modal.js
+++ rsa_connect/src/components/modal.js
@@ -21,22 +21,24 @@ function NewModal({ isOpen, offre, onClose }) {
     };
   }, [isOpen, handleOverlayClick]);
 
+  const modalClass = isOpen ? 'fixed inset-0 z-50 opacity-100' : 'fixed inset-0 z-50 opacity-0 invisible';
+
   return (
     <div>
       <div
-        className={`fixed inset-0 z-50 ${isOpen ? 'opacity-100' : 'opacity-0 invisible'}`}
+        className={modalClass}
         style={{ transition: 'opacity 0.3s' }}
       >
         <div className="fixed inset-0 flex items-center justify-center bg-black opacity-50" onClick={onClose}></div>
         <div
           ref={modalRef}
-          className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-8 shadow-md rounded-lg max-w-screen-sm flex flex-col justify-between transition-transform"
+          className={`fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-8 shadow-md rounded-lg max-w-screen-sm flex flex-col justify-between transition-transform sm:w-full`}
           style={{
             maxHeight: '80vh',
             overflowY: 'auto',
           }}
         >
-          <h2 className="text-2xl font-bold mb-6" style={{ color: '#000091' }}>
+          <h2 className="text-2xl font-bold text-center mb-6" style={{ color: '#000091' }}> {/* Ajoutez la classe text-center */}
             {offre.titre}
           </h2>
           <div className="description text-lg mb-6">
@@ -48,14 +50,13 @@ function NewModal({ isOpen, offre, onClose }) {
           >
             Fermer
           </button>
-                <button
-        onClick={onClose} 
-        style={{ backgroundColor: '#000091', color: '#ffffff' }}
-        className="mt-2 px-6 py-3 rounded-md self-center"
-      >
-        Postuler
-      </button>
-
+          <button
+            onClick={onClose}
+            style={{ backgroundColor: '#000091', color: '#ffffff' }}
+            className="mt-2 px-6 py-3 rounded-md self-center"
+          >
+            Postuler
+          </button>
         </div>
       </div>
     </div>
